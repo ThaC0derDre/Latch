2bbcb06cabc9584bc21c27880d9e9f0d9bc6a5e3
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-18 22:07:24 -0700

    Change messages. Change button fonts

--- Latched/Latched/ContentView.swift
+++ Latched/Latched/ContentView.swift
@@ -31,95 +31,90 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-        VStack {
-            Form{
-                
-                if timeCounting {
-                Text("Latched at \(startTime)")
-                }
-                
-                Section {
+            VStack {
+                Form{
                     if timeCounting {
-                        Text(minsPassed == "0" ? "Counting Minutes..." : "Minutes passed: \(minsPassed)")
+                        Text("Latched at \(startTime)")
                     }
-                    if timerStopped {
-                        Text(timeDifference == "0" ? "Zero minutes passed that time." : "Total: \(timeDifference) minutes")
-                    }
-                }
-                
-                
-                Section {
-                    Button("View Previous Times") {
-                    }
-                    .onTapGesture {
-                        showTimeView.toggle()
-                    }
-                }
-                .sheet(isPresented: $showTimeView) {
-                    TrackTimeView()
-                        .environmentObject(realmManager)
                     
-                }
-            }
-            
-            .preferredColorScheme(.light)
-            .onAppear {
-                UITableView.appearance().backgroundColor = UIColor.clear
-                UITableViewCell.appearance().backgroundColor = UIColor.clear
-            }
-            
-            
-            ZStack(alignment:.bottom) {
-            if timeCounting{
-            AnimatedButton()
-                Button("End Time") {
-                    timeCounting    = false
-                    timerStopped    = true
-                    endTime         = tFormatter.getCurrentTime()
-                    theDate         = tFormatter.getCurrentDate()
-                    timeDifference  = tFormatter.findDateDiff(time1Str: startTime, time2Str: endTime)
+                        if timeCounting {
+                            Text(minsPassed == "0" ? "Counting Minutes..." : "Minutes passed: \(minsPassed)")
+                        }
+                        if timerStopped {
+                            Text(timeDifference == "0" ? "Zero minutes passed that time." : "Total: \(timeDifference) minutes")
+                        }
                     
-                    if timeDifference != "" {
-                        realmManager.addTime(lastFed: endTime, duration: timeDifference, currentDate: theDate)
-                    }
-                }
-                    .padding(65)
-                    .foregroundColor(.white)
-                    .onReceive(timer) { _ in
-                        if !timeCounting {
-                            self.timer.upstream.connect().cancel()
-                            return
-                        }else {
-                            endTime = tFormatter.getCurrentTime()
-                            minsPassed = tFormatter.findDateDiff(time1Str: startTime, time2Str: endTime)
-
+                    
+                    Section {
+                        Button("View Previous Times") {
+                        }
+                        
+                        .onTapGesture {
+                            showTimeView.toggle()
                         }
                     }
-            }else{
-                UnanimatedButton()
-                Button("Start Timer") {
-                    timerStopped    = false
-                    startTime = tFormatter.getCurrentTime()
-                    guard !timeCounting else { return }
-                    timeCounting.toggle()
-                    self.timer = Timer.publish(every: 0, on: .main, in: .common).autoconnect()
+                    .sheet(isPresented: $showTimeView) {
+                        TrackTimeView()
+                            .environmentObject(realmManager)
+                    }
                 }
-                .padding(65)
-                .foregroundColor(.white)
+                .preferredColorScheme(.light)
+                .onAppear {
+                    UITableView.appearance().backgroundColor = UIColor.clear
+                    UITableViewCell.appearance().backgroundColor = UIColor.clear
+                }
+                
+                
+                ZStack(alignment:.bottom) {
+                    
+                    if timeCounting{
+                        AnimatedButton()
+                        Button("End Time") {
+                            timeCounting    = false
+                            timerStopped    = true
+                            endTime         = tFormatter.getCurrentTime()
+                            theDate         = tFormatter.getCurrentDate()
+                            timeDifference  = tFormatter.findDateDiff(time1Str: startTime, time2Str: endTime)
+                            
+                            if timeDifference != "" {
+                                realmManager.addTime(lastFed: endTime, duration: timeDifference, currentDate: theDate)
+                            }
+                        }
+                        .font(.body.bold())
+                        .padding(65)
+                        .foregroundColor(.white)
+                        .onReceive(timer) { _ in
+                            if !timeCounting {
+                                self.timer.upstream.connect().cancel()
+                                return
+                            }else {
+                                endTime = tFormatter.getCurrentTime()
+                                minsPassed = tFormatter.findDateDiff(time1Str: startTime, time2Str: endTime)
+                            }
+                        }
+                    }else{
+                        UnanimatedButton()
+                        Button("Start Timer") {
+                            timerStopped    = false
+                            startTime = tFormatter.getCurrentTime()
+                            guard !timeCounting else { return }
+                            timeCounting.toggle()
+                            self.timer = Timer.publish(every: 0, on: .main, in: .common).autoconnect()
+                        }
+                        .font(.body.bold())
+                        .padding(65)
+                        .foregroundColor(.white)
+                    }
+                }
             }
-            }
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+            .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
+            .navigationTitle("LATCHED")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
-        .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
-        .navigationTitle("LATCHED")
-        .navigationBarTitleDisplayMode(.inline)
-            
-    }
-        
     }
 }
-    
+
 
 
 
